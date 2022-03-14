fbad5c3cb1eb93491146ecdb6615927003849c17
alfonsopizarro <alfonsopizarroserrano94@gmail.com>
2022-03-14 19:48:03 +0100

    clean main

--- app/src/main/java/com/example/financebank/MainActivity.kt
+++ app/src/main/java/com/example/financebank/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.financebank
-
-import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import android.media.MediaPlayer
-import android.app.Activity
-import android.content.pm.PackageManager
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +13,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-
 
 
         val toast2 = Toast.makeText(
